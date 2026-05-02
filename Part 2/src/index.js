e6b02a3c1e471145bba38b4f2e89dd1383da1fe5
CodeMechanics <sa@CodeMechanics.edu>
2026-05-02 17:26:27 +0800

    Sample Part 2

--- Part 2/src/index.js
+++ Part 2/src/index.js
@@ -1,11 +1,12 @@
 'use strict'
 
+import router from './js/router.js';
 
-// eslint-disable-next-line no-unused-vars
 const app1 = new Vue({
     el: '#app',
     components: {
         'mycomponent': httpVueLoader('./components/mycomponent.vue'),
+        'myheader': httpVueLoader('./components/myheader.vue')
     },
     data(){
         return { };
@@ -16,9 +17,11 @@ const app1 = new Vue({
     }, 
     mounted: function(){
         
+        //navigates to home page at mount
+        this.$router.push('/home'); 
         uibuilder.start()
         var vueApp = this
-        uibuilder.onChange('msg', function(newVal) {
-        });
+        uibuilder.onChange('msg',function(newVal){});
     },
+    router: new VueRouter(router),
 })
